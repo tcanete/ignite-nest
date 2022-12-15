c21f3d882df0cd2ad6ae5bc86420a9459244a040
Thiago Canete <thiago.canete@gmail.com>
2022-12-15 20:27:53 -0300

    aula 2

---
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { Notification } from "../../../../application/entities/notification";
+import { NotificationsRepository } from "../../../../application/repositories/notification-repository";
+import { PrismaService } from "../prisma.service";
+
+@Injectable()
+export class PrismaNotificationsRepository implements NotificationsRepository {
+    constructor(private prismaService: PrismaService) { }
+
+    async create(notification: Notification): Promise<void> {
+        await this.prismaService.notification.create({
+            data: {
+                id: notification.id,
+                recipientId: notification.recipientId,
+                content: notification.content.value,
+                category: notification.category,
+                createdAt: notification.createdAt,
+                readAt: notification.readAt
+            }
+        });
+    }
+
+}
